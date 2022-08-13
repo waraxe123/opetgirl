8e74284c97ee2c95f2132501278d89878162cb7b
Malitha Mihiranga <iSmart_Boi@outlook.com>
2022-08-13 18:13:32 +0530

    finish!

---
+++ startup.sh
@@ -0,0 +1,53 @@
+echo ">> FETCHING UPSTREAM..."
+
+git clone https:/Rishbropromax/github.com//Cat-Girl-Bot /Cat-girl-Bot
+
+echo ">> INSTALLING REQUIREMENTS..."
+
+cd /Cat-Girl-Bot
+
+pip3 install -U -r requirements.txt
+
+echo ">> STARTING MUSIC PLAYER USERBOT..."
+
+clear
+
+echo "
+
+#     #                        
+
+##   ## #    #  ####  #  ####  
+
+# # # # #    # #      # #    # 
+
+#  #  # #    #  ####  # #      
+
+#     # #    #      # # #      
+
+#     # #    # #    # # #    # 
+
+#     #  ####   ####  #  ####  
+
+                               
+
+######                                    
+
+#     # #        ##   #   # ###### #####  
+
+#     # #       #  #   # #  #      #    # 
+
+######  #      #    #   #   #####  #    # 
+
+#       #      ######   #   #      #####  
+
+#       #      #    #   #   #      #   #  
+
+#       ###### #    #   #   ###### #    # 
+
+                                          
+
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+
+"
+
+python3 main.py
