e8041adfeb83ab2a43af3bc920310f7e8cb243b4
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-13 23:24:45 +0530

    Update hmm.py

--- CatXGirl/modules/hmm.py
+++ CatXGirl/modules/hmm.py
@@ -1,1 +1,1 @@
-"  "
+" massege For malitha :-    මලිත මම ගොඩක් හැදුවා ඔයා මෙන්න මේ [url](https://github.com/AnonymousR1025/FallenRobot/tree/master/FallenRobot/modules) එකේ තියන Files ටික add කරන්න CatxGirl වල [modules](https://github.com/RishBropromax/Cat-Girl-Bot/tree/main/CatXGirl/modules) වල upload කරන එඩිට් කරන්න මෙන්න Updates ආණ්ඩ news channel එක  @CatXGirlNews ඔය support group එකක් හදලා එක public කරලා username එක දාන්න @CatXGirlChat කියල දාන්න... ඒ ඔයාට තියෙන්නේ modules files ටික edit කරන්න විතරයි "
