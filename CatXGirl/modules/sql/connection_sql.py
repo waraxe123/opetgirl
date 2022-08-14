073e80b5a58ef521f6be61a08279e21c317e4a5a
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 13:40:05 +0530

    Update connection_sql.py

--- CatXGirl/modules/sql/connection_sql.py
+++ CatXGirl/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from FallenRobot.modules.sql import SESSION, BASE
+from CatXGirl.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
