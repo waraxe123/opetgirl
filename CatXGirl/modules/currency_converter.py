cc08ec0fcfa1ec234e17f139cd6a960d36cc2664
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:00:53 +0530

    Update currency_converter.py

--- CatXGirl/modules/currency_converter.py
+++ CatXGirl/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from FallenRobot import CASH_API_KEY, dispatcher
+from CatXGirl import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
