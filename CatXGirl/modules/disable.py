c708a6a423242205912d0aac56059fb3a61a145a
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:09:27 +0530

    Update disable.py

--- CatXGirl/modules/disable.py
+++ CatXGirl/modules/disable.py
@@ -25,7 +25,7 @@ if is_module_loaded(FILENAME):
         is_user_admin,
         user_admin,
     )
-    from FallenRobot.modules.sql import disable_sql as sql
+    from CatXGirl.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
