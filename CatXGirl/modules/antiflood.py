0d6e843f51db73acc9b2543f2eb69eeba23e30f0
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 14:06:10 +0530

    Update antiflood.py

--- CatXGirl/modules/antiflood.py
+++ CatXGirl/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from FallenRobot import TIGERS, WOLVES, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from CatXGirl import TIGERS, WOLVES, dispatcher
+from CatXGirl.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql import antiflood_sql as sql
+from CatXGirl.modules.log_channel import loggable
+from CatXGirl.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.sql.approve_sql import is_approved
+from CatXGirl.modules.helper_funcs.string_handling import extract_time
+from CatXGirl.modules.connection import connected
+from CatXGirl.modules.helper_funcs.alternate import send_message
+from CatXGirl.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
