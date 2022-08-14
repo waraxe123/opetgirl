94250f6e3e9cbd7a5ca63cd1e089fdba5636517a
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:03:43 +0530

    Update dbcleanup.py

--- CatXGirl/modules/dbcleanup.py
+++ CatXGirl/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import FallenRobot.modules.sql.global_bans_sql as gban_sql
-import FallenRobot.modules.sql.users_sql as user_sql
-from FallenRobot import DEV_USERS, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import CatXGirl.modules.sql.global_bans_sql as gban_sql
+import CatXGirl.modules.sql.users_sql as user_sql
+from CatXGirl import DEV_USERS, OWNER_ID, dispatcher
+from CatXGirl.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
