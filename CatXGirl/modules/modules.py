5d97cd9f00519907cad9ad54a95bc41eba1fa155
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:47:30 +0530

    Update modules.py

--- CatXGirl/modules/modules.py
+++ CatXGirl/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from FallenRobot import dispatcher, telethn
-from FallenRobot.__main__ import (
+from CatXGirl import dispatcher, telethn
+from CatXGirl.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from FallenRobot.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from CatXGirl.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -28,7 +28,7 @@ def load(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("FallenRobot.modules." + text)
+        imported_module = importlib.import_module("CatXGirl.modules." + text)
     except:
         load_messasge.edit_text("Does that module even exist?")
         return
@@ -98,7 +98,7 @@ def unload(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("FallenRobot.modules." + text)
+        imported_module = importlib.import_module("CatXGirl.modules." + text)
     except:
         unload_messasge.edit_text("Does that module even exist?")
         return
@@ -168,7 +168,7 @@ def listmodules(update: Update, context: CallbackContext):
     for helpable_module in HELPABLE:
         helpable_module_info = IMPORTED[helpable_module]
         file_info = IMPORTED[helpable_module_info.__mod_name__.lower()]
-        file_name = file_info.__name__.rsplit("FallenRobot.modules.", 1)[1]
+        file_name = file_info.__name__.rsplit("CatXGirl.modules.", 1)[1]
         mod_name = file_info.__mod_name__
         module_list.append(f"- <code>{mod_name} ({file_name})</code>\n")
     module_list = "Following modules are loaded : \n\n" + "".join(module_list)
