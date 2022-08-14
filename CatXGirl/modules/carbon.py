2661ba3e0cc8c784ce4b8e57bb37607be14aa96a
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 14:56:20 +0530

    Update carbon.py

--- CatXGirl/modules/carbon.py
+++ CatXGirl/modules/carbon.py
@@ -1,15 +1,15 @@
 from pyrogram.types import Message
 from pyrogram import filters
-from FallenRobot import pbot
-from FallenRobot.utils.errors import capture_err
-from FallenRobot.utils.functions import make_carbon
+from CatXGirl import pbot
+from CatXGirl.utils.errors import capture_err
+from CatXGirl.utils.functions import make_carbon
 
 
 @pbot.on_message(filters.command("carbon"))
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("`ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.`")
+        return await message.reply_text("`ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ. Wait Some Time..`")
     if not message.reply_to_message.text:
         return await message.reply_text("`ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.`")
     m = await message.reply_text("😴`ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...`")
