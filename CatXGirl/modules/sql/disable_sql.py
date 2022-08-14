40bdcabde55c5489374cfc5694937499e0e1db85
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 13:41:00 +0530

    Update disable_sql.py

--- CatXGirl/modules/sql/disable_sql.py
+++ CatXGirl/modules/sql/disable_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from FallenRobot.modules.sql import BASE, SESSION
+from CatXGirl.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
