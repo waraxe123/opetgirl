313ce825ad83670950adea6430eae36a213d9ded
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 16:24:38 +0530

    Update whois.py

--- CatXGirl/modules/whois.py
+++ CatXGirl/modules/whois.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import Message, User
 
-from FallenRobot import pbot
+from CatXGirl import pbot
 
 
 def ReplyCheck(message: Message):
