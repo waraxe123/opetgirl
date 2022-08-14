23dfe0d0973364a2e8a7d636ab0db81cf4e5abd5
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:41:54 +0530

    Update log_channel.py

--- CatXGirl/modules/log_channel.py
+++ CatXGirl/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from FallenRobot.modules.helper_funcs.misc import is_module_loaded
+from CatXGirl.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from FallenRobot import EVENT_LOGS, LOGGER, dispatcher
-    from FallenRobot.modules.helper_funcs.chat_status import user_admin
-    from FallenRobot.modules.sql import log_channel_sql as sql
+    from CatXGirl import EVENT_LOGS, LOGGER, dispatcher
+    from CatXGirl.modules.helper_funcs.chat_status import user_admin
+    from CatXGirl.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
@@ -204,7 +204,7 @@ Setting the log channel is done by:
 ❍ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "Lᴏɢs​"
+    __mod_name__ = "Lᴏɢs"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
