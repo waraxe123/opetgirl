dd084d8218a3518432301e214355306a133ff2c0
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:32:18 +0530

    Update heroku.py

--- CatXGirl/modules/heroku.py
+++ CatXGirl/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from FallenRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from FallenRobot.events import register
+from CatXGirl import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from CatXGirl.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
