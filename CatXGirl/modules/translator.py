ac18d2c150898128eb8561c6610a2b2cc7849053
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 16:09:39 +0530

    Update translator.py

--- CatXGirl/modules/translator.py
+++ CatXGirl/modules/translator.py
@@ -1,8 +1,8 @@
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
 from gpytranslate import SyncTranslator
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from CatXGirl import dispatcher
+from CatXGirl.modules.disable import DisableAbleCommandHandler
 
 trans = SyncTranslator()
 
@@ -12,10 +12,10 @@ def totranslate(update: Update, context: CallbackContext) -> None:
     reply_msg = message.reply_to_message
     if not reply_msg:
         message.reply_text(
-            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
+            "Reply to messages or write messages from other languages for translating into the intended language\n\n"
             "Example: `/tr en-hi` to translate from English to Hindi\n"
             "Or use: `/tr en` for automatic detection and translating it into english.\n"
-            "Click here to see [List of available Language Codes](https://t.me/DevilsHeavenMF/148391).",
+            ".",
             parse_mode="markdown",
             disable_web_page_preview=True,
         )
