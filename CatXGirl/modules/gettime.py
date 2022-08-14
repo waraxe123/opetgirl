887fb7bceb71988faf1934dae364c184921fd654
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:26:46 +0530

    Update gettime.py

--- CatXGirl/modules/gettime.py
+++ CatXGirl/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from FallenRobot import TIME_API_KEY, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from CatXGirl import TIME_API_KEY, dispatcher
+from CatXGirl.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
