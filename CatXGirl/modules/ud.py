fe13c57a502f6849aa8ecb6dc2e32ae1d5d9a88c
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 16:11:41 +0530

    Update ud.py

--- CatXGirl/modules/ud.py
+++ CatXGirl/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from CatXGirl import dispatcher
+from CatXGirl.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
