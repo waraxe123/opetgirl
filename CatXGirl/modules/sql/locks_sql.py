ca3fb76e8321c907aac6e95ea2a990d88e210a90
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 13:42:10 +0530

    Update locks_sql.py

--- CatXGirl/modules/sql/locks_sql.py
+++ CatXGirl/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from FallenRobot.modules.sql import SESSION, BASE
+from CatXGirl.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
