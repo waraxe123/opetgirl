407a8e06a51e9d0e5f7a7f572d8dfcf9781a6c2b
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:21:59 +0530

    Update eval.py

--- CatXGirl/modules/eval.py
+++ CatXGirl/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from CatXGirl import LOGGER, dispatcher
+from CatXGirl.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -76,7 +76,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "FallenRobot/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "CatXGirl/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
