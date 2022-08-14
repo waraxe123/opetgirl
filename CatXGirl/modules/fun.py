60938e2c662c819bf4dafe7e8b2a7faec5648222
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:25:33 +0530

    Update fun.py

--- CatXGirl/modules/fun.py
+++ CatXGirl/modules/fun.py
@@ -2,11 +2,11 @@ import html
 import random
 import time
 
-import FallenRobot.modules.fun_strings as fun_strings
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import is_user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_user
+import CatXGirl.modules.fun_strings as fun_strings
+from CatXGirl import dispatcher
+from CatXGirl.modules.disable import DisableAbleCommandHandler
+from CatXGirl.modules.helper_funcs.chat_status import is_user_admin
+from CatXGirl.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
