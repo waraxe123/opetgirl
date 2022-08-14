dc91fae193e146a3b0fbf0060324bfdbcd6fa968
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:31:28 +0530

    Update gps.py

--- CatXGirl/modules/gps.py
+++ CatXGirl/modules/gps.py
@@ -1,8 +1,8 @@
 import os
-from FallenRobot import telethn as tbot
+from CatXGirl import telethn as tbot
 from geopy.geocoders import Nominatim
-from FallenRobot.events import register
-from FallenRobot import *
+from CatXGirl.events import register
+from CatXGirl import *
 from telethon import *
 from telethon.tl import *
 
