caed4e9e73d943e5badf27fd3919028e836a7f22
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 14:51:39 +0530

    Update blacklist.py

--- CatXGirl/modules/blacklist.py
+++ CatXGirl/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.blacklist_sql as sql
-from FallenRobot import dispatcher, LOGGER
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
+import CatXGirl.modules.sql.blacklist_sql as sql
+from CatXGirl import dispatcher, LOGGER
+from CatXGirl.modules.disable import DisableAbleCommandHandler
+from CatXGirl.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from CatXGirl.modules.helper_funcs.extraction import extract_text
+from CatXGirl.modules.helper_funcs.misc import split_message
+from CatXGirl.modules.log_channel import loggable
+from CatXGirl.modules.warns import warn
+from CatXGirl.modules.helper_funcs.string_handling import extract_time
+from CatXGirl.modules.connection import connected
+from CatXGirl.modules.sql.approve_sql import is_approved
+from CatXGirl.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
