63d6ed21ffe4b94bb9a2ce4a23dc258c4385b010
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 14:53:42 +0530

    Update blacklistusers.py

--- CatXGirl/modules/blacklistusers.py
+++ CatXGirl/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import FallenRobot.modules.sql.blacklistusers_sql as sql
-from FallenRobot import (
+import CatXGirl.modules.sql.blacklistusers_sql as sql
+from CatXGirl import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from FallenRobot import (
     WOLVES,
     dispatcher,
 )
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
-from FallenRobot.modules.helper_funcs.extraction import (
+from CatXGirl.modules.helper_funcs.chat_status import dev_plus
+from CatXGirl.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from FallenRobot.modules.log_channel import gloggable
+from CatXGirl.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
