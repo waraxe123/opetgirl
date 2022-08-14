c75b94ba535e31fbc828acfc0308f6b40d38b44d
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-14 15:04:28 +0530

    Update debug.py

--- CatXGirl/modules/debug.py
+++ CatXGirl/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from FallenRobot import telethn, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from CatXGirl import telethn, dispatcher
+from CatXGirl.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
