856ecab9159e352e459e428cdc6a463a8fbdfe07
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-21 16:22:48 +0530

    Create Mod.py

---
+++ Mod.py
@@ -0,0 +1,32 @@
+from CatXGirl import app
+
+from CatXGirl.cat.commands import *
+
+@app.on_message(command("/start"))
+
+async def plug(_, message):
+
+    CatXGirl = await message.reply_text(text="🐱"  
+          CatXGirl = await message.reply_text(text="My Creators 🇱🇰 \n\n @ImRishmika \n @xMalitha"  
+
+ 
+
+    )
+
+    ImRishmika = """
+
+I'm a group management bot with some useful features.
+
+@CatXGirl_Bot
+
+    """
+
+    await CatXGirl.edit_text(ImRishmika)
+
+__MODULE__ = "Start"
+
+__HELP__ = """  
+
+/start - Owers
+
+"""
