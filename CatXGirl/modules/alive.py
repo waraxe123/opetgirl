0854a1d9a45914908f3496ddd33b24bc4f5b3b31
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2022-12-13 04:29:01 +0700

    Add files via upload

---
+++ CatXGirl/modules/alive.py
@@ -0,0 +1,36 @@
+import os
+import re
+import random
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from CatXGirl.events import register
+from CatXGirl import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatcher
+
+
+PHOTO = [
+    "https://telegra.ph/file/e92d6e9307c341661a50a.jpg",
+    "https://telegra.ph/file/e92d6e9307c341661a50a.jpg",
+]
+
+
+@register(pattern=("/alive"))
+async def awake(event):
+    TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ CatXGirl**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ : [ImRishmika](https://t.me/ImRishmika) | [Malith](t.me/xMalith)** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("ʜᴇʟᴘ", f"https://t.me/{dispatcher.bot.username}?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ", f"https://t.me/{SUPPORT_CHAT}"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
+    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+__mod_name__ = "Aʟɪᴠᴇ"
