96af4e2034bd00bd5efa00486fcdc60e293d2e2b
waraxe123 <116478751+waraxe123@users.noreply.github.com>
2022-12-13 04:41:44 +0700

    Update sample_config.py

--- CatXGirl/sample_config.py
+++ CatXGirl/sample_config.py
@@ -14,27 +14,27 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "5171347305"  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "ImRishmika Or xmalitha"
-    SUPPORT_CHAT = "Malith make Support Chat And Public it Example @CatxGirlChat"  # Your own group for support, do not add the @
+    API_ID = 25924955  # integer value, dont use ""
+    API_HASH = "140d8d0ad7c45e934d82dfcbba019e0d"
+    TOKEN = "5805856990:AAHixt2Cm-elIJAjDSmAqcEVx22CQfePUl8"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "1784179805"  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "broken or stupid"
+    SUPPORT_CHAT = "satpamajg"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001686672798
+        -1001873253940
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001686672798
+        -1001873253940
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgresql://uranhezt:wNvf2yWqH23GlkPAlzEMkavHdg954DnI@arjuna.db.elephantsql.com:5432/uranhezt"  # needed for any database modules
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "5K2DHwcQV9aWSH3ApA~Ywm4jk1wDZF7YME~p~twPxXkiK8ASe2ypp8UH9g8egsoh"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -58,9 +58,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "MTUCS6JPUFR8XRPO"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "EYO2G96ONPI6"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
